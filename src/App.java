c05fbe29e14c225abfe0f19a47974c37e451be3a
Brandon Suckerman <brandonsuckie@gmail.com>
2025-11-11 10:16:51 -0500

    done
    done

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 /**
     * You are going to write code to store a list of participant names for a 
     * local event. You will create an ArrayList for Strings, then accept user 
@@ -23,9 +24,35 @@
     * ignored, and no change is made to the list.
 */
 import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            if (!list.contains(input)) {
+                list.add(input);
+            } else {
+                System.out.println("This name has already been entered");
+                
+                // System.out.println("Do you want to keep this name?");
+                // String answer = scanner.nextLine();
+                // if (answer == "yes") {
+                //     list.remove(input);
+                // } else { continue;}.    This is my attempt for the A+
+                 
+            }
+        }
+
+        System.out.println("");
+        for (String word : list) {
+            System.out.println(word);
+        }
+
     }
 }
